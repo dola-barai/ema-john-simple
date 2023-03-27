4cdf80608bfe7f1a34fa25091922b442726c4948
Dola Barai <dola43dc@gmail.com>
2023-03-27 18:59:43 +0600

    product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,8 @@ import './Product.css';
 
 const Product = (props) => {
     const {img, name, seller, ratings, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +14,7 @@ const Product = (props) => {
             <p>Manufactures: {seller}</p>
             <p>Rating: {ratings} stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
